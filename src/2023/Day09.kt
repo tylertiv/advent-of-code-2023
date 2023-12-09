995cb721a20bfd1b590f7cd3b6b1e98ceba9a433
Tyler Tivadar <tylertivadar@gmail.com>
2023-12-09 15:36:24 -0500

    day 9 formatting

--- src/2023/Day09.kt
+++ src/2023/Day09.kt
@@ -3,24 +3,20 @@ fun main() {
         input.map { pattern -> pattern.split(" ").map { it.toLong() } }
 
     fun List<Long>.predictNext(): Long {
-        if (this.all { it == 0.toLong() }) {
+        if (this.all { it == 0.toLong() })
             return 0
-
-        }
         return this.last() + this.zipWithNext { first, second -> second - first }.predictNext()
     }
 
     fun List<Long>.predictPrevious(): Long {
-        if (this.all { it == 0.toLong() }) {
+        if (this.all { it == 0.toLong() })
             return 0
-
-        }
         return this.first() - this.zipWithNext { first, second -> second - first }.predictPrevious()
     }
 
-    fun part1(input: List<String>) = parsePatterns(input).also { println(it) }.sumOf { it.predictNext() }
+    fun part1(input: List<String>) = parsePatterns(input).sumOf { it.predictNext() }
 
-    fun part2(input: List<String>) = parsePatterns(input).also { println(it) }.sumOf { it.predictPrevious() }
+    fun part2(input: List<String>) = parsePatterns(input).sumOf { it.predictPrevious() }
 
 
     val input = readInput("src/2023/input/Day09")
